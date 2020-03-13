e1f8522108f2cc5106a654eb262eba6032f93f26
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-13 17:28:32 +0530

    addedNewFeature

--- app.js
+++ app.js
@@ -23,50 +23,44 @@ const itemsSchema = new mongoose.Schema({
   name: String
 });
 
-const item = mongoose.model("item", itemsSchema);
+const Item = mongoose.model("item", itemsSchema);
 
-const item1 = new item({
+const item1 = new Item({
   name: "Welcome to your ToDoList"
 });
 
-const item2 = new item({
+const item2 = new Item({
   name: "Hit the + button to add a new item"
 });
 
-const item3 = new item({
+const item3 = new Item({
   name: "Hit this to delete an item"
 });
 
 const defaultItem = [item1, item2, item3];
+//
 
-// item.insertMany(defaultItem, function(err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Work successful");
-//   }
-// });
 // --------->
 
 app.get("/", function(req, res) {
 
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItem, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Work successful");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        newListItems: foundItems,
+        listTitle: "toDoList"
+      });
+    }
   });
-});
-
-
-app.post("/", function(req, res) {
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
 });
 
 app.get("/work", function(req, res) {
@@ -79,6 +73,30 @@ app.get("/work", function(req, res) {
 app.get("/about", function(req, res) {
   res.render("about");
 });
+
+
+app.post("/", function(req, res) {
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res) {
+  const checkedItem = req.body.checkBox;
+  Item.findByIdAndRemove(checkedItem, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("successfully removed");
+      res.redirect("/");
+    }
+  });
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
